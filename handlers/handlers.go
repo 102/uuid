bd22322fe2fed9010ded185a13b9680ddbf2c5dc
Roman Gusev <rgusev2@gmail.com>
2024-07-02 12:02:58 +0200

    Log method

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -8,16 +8,8 @@ import (
 	"github.com/google/uuid"
 )
 
-const sourceHeaderName string = "X-Uuid-Request-Source"
-const defaultSourceValue string = "unknown"
-
 func RootHandler(w http.ResponseWriter, r *http.Request) {
-	source := defaultSourceValue
-	header := r.Header[sourceHeaderName]
-	if len(header) > 0 {
-		source = header[0]
-	}
 	res := uuid.New().String()
-	log.Printf("uuid generated: %s; source: %s\n", res, source)
+	log.Printf("uuid generated: %s; method: %s\n", res, r.Method)
 	fmt.Fprintf(w, "%s", res)
 }
